b535c63b967d8dc8043d3c3089ebf7b7cdcb1d2e
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-06-04 16:03:32 +0300

    Add openBottomSheetButton to MainViewController

--- MyBottomSheer/MyBottomSheer/MainViewController.swift
+++ MyBottomSheer/MyBottomSheer/MainViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - Private properties
+
+    private let openBottomSheetButton = UIButton()
+
     // MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        self.configureBottomSheetButton()
+    }
+
+    // MARK: - Actions
+
+    @objc
+    private func openBottomSheet() {
+
+    }
+
+    // MARK: - Private methods
+
+    func configureBottomSheetButton() {
+        self.view.addSubview(self.openBottomSheetButton)
+        self.openBottomSheetButton.setTitle("Open Bottom Sheet", for: .normal)
+        self.openBottomSheetButton.backgroundColor = .blue
+        self.openBottomSheetButton.layer.cornerRadius = 5
+        self.openBottomSheetButton.translatesAutoresizingMaskIntoConstraints = false
+        self.openBottomSheetButton.addTarget(self, action: #selector(openBottomSheet), for: .touchUpInside)
+        NSLayoutConstraint.activate([
+            self.openBottomSheetButton.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            self.openBottomSheetButton.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+            self.openBottomSheetButton.widthAnchor.constraint(equalToConstant: 200),
+            self.openBottomSheetButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
     }
 }
 
