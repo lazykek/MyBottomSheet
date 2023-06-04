7aab55246ac5c82cb0e64ebe24b7b9b20984f223
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-06-04 16:54:03 +0300

    Add BottomSheetPresentationController

---
+++ MyBottomSheet/MyBottomSheet/BottomSheet/BottomSheetPresentationController.swift
@@ -0,0 +1,11 @@
+//
+//  BottomSheetPresentationController.swift
+//  MyBottomSheet
+//
+//  Created by Ilya Cherkasov on 04.06.2023.
+//
+
+import UIKit
+
+final class BottomSheetPresentationController: UIPresentationController {
+}
--- MyBottomSheet/MyBottomSheet/BottomSheet/BottomSheetTransitioningDelegate.swift
+++ MyBottomSheet/MyBottomSheet/BottomSheet/BottomSheetTransitioningDelegate.swift
@@ -8,4 +8,14 @@
 import UIKit
 
 final class BottomSheetTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
+    func presentationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController?,
+        source: UIViewController
+    ) -> UIPresentationController? {
+        BottomSheetPresentationController(
+            presentedViewController: presented,
+            presenting: presenting
+        )
+    }
 }
