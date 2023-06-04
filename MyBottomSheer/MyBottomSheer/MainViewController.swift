154a7af5501752bf1e5e54694ad7599abc53f568
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-06-04 15:48:43 +0300

    Rename ViewController to MainViewController

--- MyBottomSheer/MyBottomSheer/MainViewController.swift
+++ MyBottomSheer/MyBottomSheer/MainViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+
+    // MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
     }
-
-
 }
 
