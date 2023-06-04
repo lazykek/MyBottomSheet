51efd12811bd1d42db5b77738f3ae943ab8c9518
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-06-04 15:34:12 +0300

    Initialise project

---
+++ MyBottomSheer/MyBottomSheer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBottomSheer
+//
+//  Created by Ilya Cherkasov on 04.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
